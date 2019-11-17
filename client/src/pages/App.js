77a64d73e2107ec3f071f16a8e4b85fa6328e2ba
blacktiny <blacktiny126@hotmail.com>
2019-11-17 06:51:21 -0800

    initial commit

---
+++ client/src/pages/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Route, BrowserRouter } from 'react-router-dom'
+import Home from './Home'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={Home} />
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default App
