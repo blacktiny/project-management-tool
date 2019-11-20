eb03e04fb7a268df13b5e37f4e88c22fd6bab7df
blacktiny <blacktiny126@hotmail.com>
2019-11-19 16:31:40 -0800

    update progressbar component style

--- client/src/components/ResizableProgressBar/index.js
+++ client/src/components/ResizableProgressBar/index.js
@@ -35,7 +35,7 @@ class ResizableProgressBar extends Component {
    */
   updateDimensions() {
     let { resizableInfo } = this.state
-    const maxWidth = window.innerWidth - 462
+    const maxWidth = window.innerWidth - 692
 
     resizableInfo.maxConstraints = [maxWidth, Infinity]
     this.setState({ maxWidth, resizableInfo })
